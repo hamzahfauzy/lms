79083bfc1c4963d60202ddc428d7c464e2e4d9e1
hamzahfauzy <hamzahfauzy97@gmail.com>
2020-04-22 18:44:54 +0700

    change tempdir

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -321,7 +321,7 @@ class SiteController extends Controller
         // setup kartik\mpdf\Pdf component
         $pdf = new Pdf([
             // set to use core fonts only
-            // 'mode' => Pdf::MODE_CORE, 
+            'mode' => Pdf::MODE_CORE, 
             // A4 paper format
             'format' => Pdf::FORMAT_A4, 
             // portrait orientation
@@ -337,6 +337,7 @@ class SiteController extends Controller
             'cssInline' => '.kv-heading-1{font-size:18px}', 
             // set mPDF properties on the fly
             'options' => ['title' => 'Panduan'],
+            'tempDir'=>Yii::getPathOfAlias('application.runtime'),
             // call mPDF methods on the fly
             'methods' => [ 
                 'SetHeader'=>[''], 
